6427e9695fda16eaf712b7bce41dcdd084b0e7b0
Marcin Maciag <01159356@pw.edu.pl>
2022-05-05 18:31:26 +0200

    Changes

--- public/js/aboutPage/observer.js
+++ public/js/aboutPage/observer.js
@@ -22,7 +22,38 @@ const showPic = function (entries, observer) {
 
 const picObserver = new IntersectionObserver(showPic, {
   root: null,
-  threshold: 0.4,
+  threshold: 0.6,
 });
 
 picObserver.observe($sportPicSection);
+
+// Make colorful and bigger title
+
+const $plusesSection = document.querySelector(".pluses");
+const $plusesTitles = document.querySelectorAll(".pluses--title");
+
+const animateTitles = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  const t2 = gsap.timeline({ paused: true });
+
+  $plusesTitles.forEach((title) => {
+    t2.to(title, {
+      color: "#42bdb3",
+      duration: 0.4,
+    }).to(title, { color: "#000000", duration: 1 });
+  });
+
+  t2.play();
+
+  observer.unobserve(entry.target);
+};
+
+const plusesObserver = new IntersectionObserver(animateTitles, {
+  root: null,
+  threshold: 0.4,
+});
+
+plusesObserver.observe($plusesSection);
--- src/index.js
+++ src/index.js
@@ -5,6 +5,22 @@ const publicDirPath = path.join(__dirname, "../public");
 
 const app = express();
 app.use(express.static(publicDirPath));
+app.set("view engine", "html");
+app.set("views", publicDirPath);
+
+app.engine("html", require("hbs").__express);
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
