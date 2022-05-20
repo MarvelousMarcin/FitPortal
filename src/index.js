7ab0ef18a6999e300713c71f31118d3b53667ef0
Marcin Maciag <01159356@pw.edu.pl>
2022-05-20 19:14:34 +0200

    Friends Interface

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
+const http = require("http");
 const express = require("express");
+const socketio = require("socket.io");
 const path = require("path");
 const port = process.env.PORT || 3000;
 const publicDirPath = path.join(__dirname, "../public");
+
 const UserRoute = require("./Routes/user");
 const BmiRoute = require("./Routes/bmi");
 const DataRoute = require("./Routes/data");
+
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const { auth, reDirToMain } = require("./Routes/auth");
 require("./mongoose");
 const dbString = "mongodb://127.0.0.1:27017/FitPortal";
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
 app.use(express.static(publicDirPath));
 app.set("view engine", "html");
 app.set("views", publicDirPath);
@@ -42,6 +50,10 @@ app.get("/data", reDirToMain, (req, res) => {
   res.render("data", { login: req.user.login });
 });
 
+app.get("/friends", reDirToMain, (req, res) => {
+  res.render("friends");
+});
+
 app.get("/register", (req, res) => {
   res.render("register");
 });
@@ -50,6 +62,6 @@ app.get("/news", (req, res) => {
   res.render("news");
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
