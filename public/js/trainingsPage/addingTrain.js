57a1dacfa45e662885b29a7556aa91f1e882f49e
MarvelousMarcin <01159356@pw.edu.pl>
2022-05-26 21:04:42 +0200

    Trainings finished

--- public/js/trainingsPage/addingTrain.js
+++ public/js/trainingsPage/addingTrain.js
@@ -7,6 +7,49 @@ const $sendReq = document.querySelector(".add-btn");
 const $nameInput = document.querySelector(".add-name");
 const $duratInput = document.querySelector(".add-duration");
 const $dateInput = document.querySelector(".add-date");
+const $trainings = document.querySelector(".trainings");
+
+const createDateFunc = (date) => {
+  const newDate = new Date(date);
+  let day = newDate.getDate();
+  let month = newDate.getMonth();
+  let year = newDate.getFullYear();
+
+  if (day < 10) {
+    day = "0" + day;
+  }
+  if (month < 10) {
+    month = "0" + month;
+  }
+
+  return `${day}.${month}.${year}`;
+};
+
+const getTimeString = (time) => {
+  const trainingHour = Math.trunc(time / 60);
+  const trainingMin = time - 60 * trainingHour;
+  const dura = `${trainingHour}h ${trainingMin}min`;
+  return dura;
+};
+
+const loadTraingins = async () => {
+  const response = await fetch("http://localhost:3000/traininglist");
+  const data = await response.json();
+
+  if (!data) {
+    return;
+  }
+
+  data.forEach((training) => {
+    const html = `<div class="training">
+    <div class="title">${training.name}</div>
+    <div class="duration">${training.dura}</div>
+    <div class="date">${createDateFunc(training.date)}</div>
+  </div>`;
+
+    $trainings.insertAdjacentHTML("afterbegin", html);
+  });
+};
 
 $addBtn.addEventListener("click", () => {
   $addBox.style.transform = "scale(1)";
@@ -34,7 +77,17 @@ $sendReq.addEventListener("click", async () => {
   });
 
   if (response.ok) {
+    const html = `<div class="training">
+    <div class="title">${name}</div>
+    <div class="duration">${getTimeString(duration)}</div>
+    <div class="date">${createDateFunc(date)}</div>
+    </div>`;
+    console.log(html);
+
+    $trainings.insertAdjacentHTML("afterbegin", html);
   }
 
   $blur.click();
 });
+
+loadTraingins();
